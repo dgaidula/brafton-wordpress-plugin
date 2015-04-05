7a428a8b22246c2e08738585e2c1a5713052aabd
Dan Gaidula <dgaidula@users.noreply.github.com>
2015-04-05 18:57:30 -0400

    Update brafton_errors.php
    Added missing display_xml_error function. Without this plug-in was not working correctly.

--- src/brafton_errors.php
+++ src/brafton_errors.php
@@ -120,4 +120,32 @@ function brafton_render_log( $priority = array( 0 ), $log_option ){
     $output .= "</ul>";
     return $output;
 }
+
+function display_xml_error($error, $xml)
+{
+    $return  = $xml[$error->line - 1] . "\n";
+    $return .= str_repeat('-', $error->column) . "^\n";
+
+    switch ($error->level) {
+        case LIBXML_ERR_WARNING:
+            $return .= "Warning $error->code: ";
+            break;
+         case LIBXML_ERR_ERROR:
+            $return .= "Error $error->code: ";
+            break;
+        case LIBXML_ERR_FATAL:
+            $return .= "Fatal Error $error->code: ";
+            break;
+    }
+
+    $return .= trim($error->message) .
+               "\n  Line: $error->line" .
+               "\n  Column: $error->column";
+
+    if ($error->file) {
+        $return .= "\n  File: $error->file";
+    }
+
+    return "$return\n\n--------------------------------------------\n\n";
+}
 ?>
